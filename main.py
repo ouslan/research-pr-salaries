f376adda394c0497deceda03ae4c831531aafc72
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2025-01-27 16:50:08 -0400

    added spatial tables for county

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from src.data.data_process import DataReg
 
+
 def main() -> None:
     d = DataReg("data/")
-    d.make_dp03_dataset()
-    d.make_spatial_table()
     d.make_qcew_dataset()
+
 
 if __name__ == "__main__":
     main()
--- src/data/data_process.py
+++ src/data/data_process.py
@@ -1,4 +1,5 @@
 from ..jp_qcew.src.data.data_process import cleanData
+from ..models import *
 import pandas as pd
 import geopandas as gpd
 import os
@@ -12,45 +13,11 @@ class DataReg(cleanData):
     ):
         super().__init__(saving_dir, database_url)
 
-    def make_dp03_dataset(self):
-        df = pd.read_stata(f"{self.saving_dir}external/zipcode.dta")
-        df["zipcode"] = df["zipcode"].astype("str").str.zfill(5)
-        self.conn.insert("dp03table", df)
-
     def semipar_data(self):
-        if "dp03table" not in self.conn.list_tables():
-            self.make_dp03_dataset()
         if "qcewtable" not in self.conn.list_tables():
             self.make_qcew_dataset()
-        if (
-            "munitable" not in self.conn.list_tables()
-            or "zipstable" not in self.conn.list_tables()
-        ):
-            self.make_spatial_table()
-
         df_qcew = self.conn.table("qcewtable")
-        df_dp03 = self.conn.table("dp03table").drop("id")
 
-        df_qcew = df_qcew.filter(
-            df_qcew.ein != "",
-            df_qcew.phys_addr_5_zip != "",
-            df_qcew.first_month_employment != "",
-            df_qcew.qtr != "",
-            df_qcew.latitude != "",
-            df_qcew.longitude != ""
-        )
-        df_qcew = df_qcew.cast(
-            {
-                "first_month_employment": "float64",
-                "second_month_employment": "float64",
-                "third_month_employment": "float64",
-                "year": "int32",
-                "qtr": "int32",
-                "total_wages": "float64",
-                "latitude": "float64",
-                "longitude": "float64",
-            }
-        )
         df_qcew = df_qcew.mutate(
             total_employment=(
                 df_qcew.first_month_employment
@@ -59,58 +26,36 @@ class DataReg(cleanData):
             )
             / 3
         )
-        # df_qcew = df_qcew.group_by(["year", "qtr", "ein", "phys_addr_5_zip"]).aggregate(
-        #     [df_qcew.total_employment.sum().name("total_employment"), df_qcew.total_wages.sum().name("total_wages")]
-        #     )
-        df_qcew = df_qcew.rename(zipcode="phys_addr_5_zip")
+        df_qcew = df_qcew.filter(~df_qcew.geom.x().isnull())
+        df_qcew = df_qcew.filter(df_qcew.geom.x() != 0)
 
-        return (
-            df_qcew  # .join(df_dp03, predicates=["year", "qtr", "zipcode"], how="left")
+        return df_qcew.select(
+            "year", "naics_code", "total_employment", "total_wages", "geom"
         )
 
     def make_spatial_table(self):
-        if not os.path.exists(f"{self.saving_dir}external/zip_shape.zip"):
-            self.pull_file(
-                url="https://www2.census.gov/geo/tiger/TIGER2024/ZCTA520/tl_2024_us_zcta520.zip",
-                filename=f"{self.saving_dir}external/zip_shape.zip",
-            )
+        # pull shape files from the census
         if not os.path.exists(f"{self.saving_dir}external/county.zip"):
             self.pull_file(
                 url="https://www2.census.gov/geo/tiger/TIGER2024/COUNTY/tl_2024_us_county.zip",
                 filename=f"{self.saving_dir}external/county.zip",
             )
-        zips = gpd.read_file(f"{self.saving_dir}external/zip_shape.zip")
-        zips = zips[zips["ZCTA5CE20"].str.startswith("00")]
-
-        gdf = gpd.read_file(f"{self.saving_dir}external/county.zip")
-        gdf = gdf[gdf["STATEFP"].str.startswith("72")].rename(
-            columns={"geometry": "count_geo"}
-        )
-
-        master_df = gpd.GeoDataFrame(columns=["ZCTA5CE20", "GEOID"])
-        for geo in gdf["GEOID"].values:
-            tmp = gdf[gdf["GEOID"] == geo]
-            tmp = zips.clip(tmp["count_geo"])
-            tmp["GEOID"] = geo
-            master_df = pd.concat(
-                [master_df, tmp[["ZCTA5CE20", "GEOID"]]], ignore_index=True
+        if not os.path.exists(f"{self.saving_dir}external/countsub.shp"):
+            self.pull_file(
+                url="https://www2.census.gov/geo/tiger/TIGER2024/COUSUB/tl_2024_72_cousub.zip",
+                filename=f"{self.saving_dir}external/countsub.zip",
             )
-        master_df = pd.merge(
-            zips,
-            master_df.drop_duplicates(subset=["ZCTA5CE20"]),
-            on="ZCTA5CE20",
-            validate="1:1",
-        )
-        master_df = pd.merge(master_df, gdf, on="GEOID", validate="m:1")
-        master_df = master_df[["GEOID", "ZCTA5CE20"]]
-        master_df = master_df.rename(
-            columns={"GEOID": "geo_id", "ZCTA5CE20": "zipcode"}
-        )
-        gdf = (
-            gdf[["GEOID", "NAME"]]
-            .rename(columns={"GEOID": "geo_id", "NAME": "municipality"})
-            .reset_index(drop=True)
-        )
 
-        self.conn.insert("zipstable", master_df)
-        self.conn.insert("munitable", gdf)
+        # initiiate the database tables
+        if "countytable" not in self.conn.list_tables():
+            init_countytable(self.data_file)
+            gdf = gpd.read_file("data/external/county.zip", engine="pyogrio")
+            gdf = gdf[gdf["STATEFP"] == "72"]
+            gdf = gdf[["GEOID", "NAME", "geometry"]].reset_index(drop=True)
+            gdf = gdf.rename(
+                columns={"GEOID": "geo_id", "NAME": "name", "geometry": "geom"}
+            )
+            gdf["geom"] = gdf["geom"].apply(lambda x: x.wkt)
+            self.conn.insert("countytable", gdf)
+        if "countsubtable" not in self.conn.list_tables():
+            init_coutsubtable(self.data_file)
