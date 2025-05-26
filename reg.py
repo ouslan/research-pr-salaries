15e0e52d853a36a69f9a91a53c29c12c0a26c638
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2025-05-25 20:33:09 -0400

    added save condition

--- reg.py
+++ reg.py
@@ -16,55 +16,54 @@ def main() -> None:
 
     dr = DataReg()
     naics_code = [
-        "11",
+        # "11",
         "21",
         "22",
-        "23",
-        "31-33",
-        "42",
-        "44-45",
-        "48-49",
-        "51",
-        "52",
-        "54",
-        "55",
-        "56",
-        "61",
-        "62",
-        "71",
-        "72-accommodation",
-        "72-food",
-        "81",
-        "92",
+        # "23",
+        # "31-33",
+        # "42",
+        # "44-45",
+        # "48-49",
+        # "51",
+        # "52",
+        # "54",
+        # "55",
+        # "56",
+        # "61",
+        # "62",
+        # "71",
+        # "72-accommodation",
+        # "72-food",
+        # "81",
+        # "92",
     ]
     for naics in naics_code:
         data = dr.regular_data(naics=naics)
+        for i in ["foreign", "local"]:
+            if not os.path.exists(f"results_{i}_{naics}.nc"):
+                if i == "foreign":
+                    data_pr = data[data["local"] == 0]
+                else:
+                    data_pr = data[data["foreign"] == 1]
+                data_pr = data[data["foreign"] == 0]
+                model = bmb.Model(
+                    "log_total_employment ~ 0 + date + ein + log_k_index + own_children6 + own_children17 + commute_car + food_stamp + with_social_security",
+                    data_pr,
+                    dropna=True,
+                )
+                results = model.fit(
+                    sample_kwargs={"nuts_sampler": "blackjax"}, cores=15
+                )
+                az.to_netcdf(results, f"data/processed/results_{i}_{naics}.nc")
+                dr.notify(
+                    url=str(os.environ.get("URL")),
+                    auth=str(os.environ.get("AUTH")),
+                    msg=f"Succesfully completed regresion for NAICS {naics} for {i}",
+                )
 
-        # PR regression
-        data_pr = data[data["foreign"] == 0]
-        model = bmb.Model(
-            "log_total_employment ~ 0 + date + ein + log_k_index + own_children6 + own_children17 + commute_car + food_stamp + with_social_security",
-            data_pr,
-            dropna=True,
-        )
-        results = model.fit(sample_kwargs={"nuts_sampler": "blackjax"}, cores=15)
-        az.to_netcdf(results, f"data/processed/results_pr_model_{naics}.nc")
-
-        # Foreign regression
-        data_pr = data[data["foreign"] == 1]
-        model = bmb.Model(
-            "log_total_employment ~ 0 + date + ein + log_k_index + own_children6 + own_children17 + commute_car + food_stamp + with_social_security",
-            data_pr,
-            dropna=True,
-        )
-        results = model.fit(sample_kwargs={"nuts_sampler": "blackjax"}, cores=15)
-        az.to_netcdf(results, f"data/processed/results_foreign_model_{naics}.nc")
-
-        dr.notify(
-            url=str(os.environ.get("URL")),
-            auth=str(os.environ.get("AUTH")),
-            msg=f"Succesfully completed regresion for NAICS {naics}",
-        )
+            else:
+                print("yeas")
+                continue
 
 
 if __name__ == "__main__":
